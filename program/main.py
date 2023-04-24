8e104087b2c1be111d524150a70e884ce46c4464
Savio Kerber <savioazure@gmail.com>
2023-04-24 19:39:31 +0200

    edit main file

--- program/main.py
+++ program/main.py
@@ -1,6 +1,11 @@
-
-
+from func_connections import connect_dydx
 # MAIN FUNCTION
 if __name__ == "__main__":
 
-  print("Hello bot!")
+   # /connect to client
+    try:
+      client = connect_dydx()
+    except Exception as e:
+      print(e)
+      print("Error connecting to client: ", e)
+      exit(1)
